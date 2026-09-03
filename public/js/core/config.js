e34974d8128b97bbc721e55a590ed72b5b256fd5
JAVIER MAESTRO <maestrojavi@yahoo.es>
2026-09-03 10:02:17 +0200

    Ahora: fallback usa window.location.origin. Antes: fallback de API en hosts no reconocidos iba a http://localhost:6001

--- public/js/core/config.js
+++ public/js/core/config.js
@@ -3,4 +3,4 @@ export const apiBaseUrl = window.location.hostname.includes('app.blockswine.com'
     ? 'https://app.blockswine.com'
     : window.location.hostname.includes('apps.run-on-seenode.com')
     ? 'https://web-sdzlt1djuiql.up-de-fra1-k8s-1.apps.run-on-seenode.com'
-    : 'http://localhost:6001';
+    : window.location.origin;
